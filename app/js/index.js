ef9d00dd4faee257f0018faf0fd9686e5b8ab381
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-18 16:49:20 -0500

    finished variables

--- app/js/index.js
+++ app/js/index.js
@@ -4,5 +4,18 @@ require('styles/main.scss');
 import $ from 'jquery';
 import { log, logTitle } from 'logger';
 /* your imports */
-logTitle('Title')
+logTitle('Variables')
 /* coding examples */
+
+//var is keyword for variable
+//js makes inference of data types
+
+var name = "Saul Tobias";
+var age = 23;
+var hasDriverLicence = true;
+var empty = undefined;
+
+log(name + " - Data Type: " + typeof(name));
+log(age + " - Data Type: " + typeof(age));
+log(hasDriverLicence + " - Data Type: " + typeof(hasDriverLicence));
+log(empty + " - Data Type: " + typeof(empty));
